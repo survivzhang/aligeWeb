66f3dd7d390e87addff2aa4c3e95feff60ad4e64
zichen <zzcnhy@gmail.com>
2025-03-25 05:28:10 +0800

    remove data to js, use "object"

---
+++ myScripts.js
@@ -0,0 +1,93 @@
+const bestPractices = [
+  {
+    id: "codeingStandard",
+    label: "Choose Industry-Specific Coding Standards",
+    description:
+      "Adhering to industry-specific coding standards in software engineering makes writing correct code that matches product expectations easier. Writing code that will satisfy the end-users and meet business requirements becomes easier.",
+    checked: false,
+  },
+  {
+    id: "codeReadability",
+    label: "Focus on Code readability",
+    description:
+      "Readable code is easy to follow and optimizes space and time.",
+    checked: false,
+  },
+  {
+    id: "meaningfulNames",
+    label: "Meaningful Names",
+    description:
+      "Choose meaningful names that convey the purpose of the variable or function. Consistent naming conventions enhance clarity and maintainability.",
+    checked: false,
+  },
+  {
+    id: "avoidSingleIdentifier",
+    label: "Avoid using a Single Identifier for multiple purposes",
+    description:
+      "Ascribe a name to each variable that clearly describes its purpose.",
+    checked: false,
+  },
+  {
+    id: "addComments",
+    label: "Add Comments and Prioritize Documentation",
+    description:
+      "Comments serve as a form of documentation within the code, explaining the logic, functionality, or purpose of specific sections. Well-placed comments transform complex algorithms or intricate business rules into understandable pieces of information.",
+    checked: false,
+  },
+  {
+    id: "efficientDataProcessing",
+    label: "Efficient Data Processing",
+    description:
+      "Divide code into smaller, self-contained modules or functions for reusability and maintainability. Identify inefficient algorithms or data structures and refactor for better performance.",
+    checked: false,
+  },
+  {
+    id: "effectiveVersionControl",
+    label: "Effective Version Control and Collaboration",
+    description:
+      "Ensure all developers follow consistent coding techniques. Use automation tools for version control workflows.",
+    checked: false,
+  },
+  {
+    id: "effectiveCodeReview",
+    label: "Effective Code Review and Refactoring",
+    description:
+      "Engage QA during refactoring to prevent new bugs. Isolate debugging from refactoring to maintain stability.",
+    checked: false,
+  },
+  {
+    id: "formalizeExceptionHandling",
+    label: "Try to formalize Exception Handling",
+    description:
+      "Exception handling involves responding to these exceptions effectively.",
+    checked: false,
+  },
+  {
+    id: "securityConsiderations",
+    label: "Security and Privacy Considerations",
+    description:
+      "Extract insights without compromising privacy. Acquire maximum insight from consented data for customer benefit.",
+    checked: false,
+  },
+  {
+    id: "standardizeHeaders",
+    label: "Standardize Headers for Different Modules",
+    description:
+      "It is easier to understand and maintain code when the headers of different modules align with a singular format.",
+    checked: false,
+  },
+  {
+    id: "turnDailyBackups",
+    label: "Turn Daily Backups into an instinct",
+    description:
+      "Multiple events can trigger data loss – system crash, dead battery, software glitch, hardware damage, etc. To prevent this, save code daily, and after every modification, no matter how minuscule it may be, back up the workflow on TFS, SVN, or any other version control mechanism.",
+    checked: false,
+  },
+  {
+    id: "chooseStandards",
+    label: "When choosing standards, think Closed vs. Open",
+    description:
+      "Consider CERT vs. MISRA. CERT emphasizes community cooperation and participation.",
+    checked: false,
+  },
+];
